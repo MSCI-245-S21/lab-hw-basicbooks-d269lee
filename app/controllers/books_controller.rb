47470180272ae408db3c09a2fa5492a8d171d9a3
Dana Lee <d269lee@uwaterloo.ca>
2021-07-01 10:16:08 -0400

    books_controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,60 +1,60 @@
 class BooksController < ApplicationController
     
  #index view should produce a list of books
-    def index
-      @books = Book.all
-  end
+        def index
+          @books = Book.order(title: :asc)
 
-    def create
-    book_params = params.require(:book).permit(:title, :author, :rating)
-    @book = Book.new(book_params)
+      end
 
-        if @book.save
-          redirect_to @book, notice: 'Book was successfully created.'
-          return
-        else
-          render :new
+        def create
+        book_params = params.require(:book).permit(:title, :author, :rating)
+        @book = Book.new(book_params)
+
+            if @book.save
+              redirect_to @book, notice: 'Book was successfully created.'
+              return
+            else
+              render :new
+            end
+      end
+
+
+       def new
+          @book = Book.new
+      end
+
+        def edit
+        @book = Book.find params[:id]
+      end
+
+
+      def show
+         @book = Book.find params[:id] 
+      end
+
+       def update
+        @book = Book.find params[:id]
+        book_params = params.require(:book).permit(:title, :author, :rating)
+            if @book.update(book_params)
+              redirect_to @book, notice: 'Book was successfully updated.'
+              return
+            else
+              render :edit
+            end
+      end
+
+        def destroy
+        @book = Book.find params[:id]
+        @book.destroy
+        redirect_to books_url, notice: 'Book was successfully destroyed.'
+      end
+
+    
+        def confirm_delete
+          @book = Book.find params[:id] 
         end
-  end
-
-
-
-
-   def new
-      @book = Book.new
-  end
-
-    def edit
-    @book = Book.find params[:id]
-  end
-
-
-  def show
-     @book = Book.find params[:id] 
-  end
-
-   def update
-    @book = Book.find params[:id]
-    book_params = params.require(:book).permit(:title, :author, :rating)
-        if @book.update(book_params)
-          redirect_to @book, notice: 'Book was successfully updated.'
-          return
-        else
-          render :edit
-        end
-  end
-
-    def destroy
-    @book = Book.find params[:id]
-    @book.destroy
-    redirect_to books_url, notice: 'Book was successfully destroyed.'
-  end
 
 end
-
-
-
-
 
 
 
